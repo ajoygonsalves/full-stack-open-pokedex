0d66557c50924df6a1940d0ef6eda9697d1552dd
ajoyous <ajoy.gonsalves@hotmail.com>
2025-02-05 21:02:27 +0100

    update text

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ErrorMessage = ({ error }) => (
-  <div data-testid="error">Hi! An error occured: {error.toString()}</div>
+  <div data-testid="error">Hi! A new error occured: {error.toString()}</div>
 );
 
 export default ErrorMessage;
